58ae613641b6acefc275cab839d14d4d4bc513f3
D.Sailesh Dev <saileshdev@gmail.com>
2016-09-12 00:04:05 +0530

    Extract image form the email using gravatar

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+
+  #returns the gravar for the given user
+  def gravatar_for(user)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{Digest::MD5::hexdigest(user.email)}"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  end
+
 end
