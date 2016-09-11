2c09c02f53e7a2603a038462b37264e60e25cd1c
D.Sailesh Dev <saileshdev@gmail.com>
2016-09-11 11:29:09 +0530

    Add helper code to display title for pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+def full_title(title = '')
+  base_title = "Microblog"
+  if title.empty?
+    base_title
+  else
+    "#{page_title} | #{base_title}"
+  end
+end
+
 end
