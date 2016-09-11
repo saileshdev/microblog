f57719b45a78345e514900bee0ba618478aefad5
D.Sailesh Dev <saileshdev@gmail.com>
2016-09-11 18:32:53 +0530

    Adds test for validating length of name attr

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -16,6 +16,12 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.valid?
   end
 
+  test "name should not be too long" do
+    @user.name = "a" * 51
+    assert_not @user.valid?
+  end
+
+
   test "email should be present" do
     @user.email = "    "
     assert_not @user.valid?
