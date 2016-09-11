7bcf2c941c83af996cca380b8997a57cd92819d1
D.Sailesh Dev <saileshdev@gmail.com>
2016-09-11 19:15:48 +0530

    Add uniqueness validation for the email attr

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ActiveRecord::Base
 
   validates :name, presence: true, length: {maximum: 50}
   validates :email, presence: true, length: {maximum: 255},
-    format: {with: VALID_EMAIL_REGEX}
+    format: {with: VALID_EMAIL_REGEX}, uniqueness: true
 
 end
