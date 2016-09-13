f538ec63f3b4aec1b1f629a4ac7b236d2239a45a
D.Sailesh Dev <saileshdev@gmail.com>
2016-09-13 21:36:57 +0530

    Display flash messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,8 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       # handle a succesful save
+      flash[:success] = "Welcome to Micro-blog!"
+      redirect_to @user
     else
       render "new"
     end 
