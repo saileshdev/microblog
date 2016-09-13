3ced294c9220a62f293c6bed364cb264a6f41be3
D.Sailesh Dev <saileshdev@gmail.com>
2016-09-13 21:43:13 +0530

    Use post_via_redirect to follow redirect after a post request

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -18,10 +18,9 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     get signup_path
    
     assert_difference 'User.count',1 do 
-    post users_path, user: {name: "Example user",email: "user@example.com", password: "password", password_confirmation: "password"}
+    post_via_redirect users_path, user: {name: "Example user",email: "user@example.com", password: "password", password_confirmation: "password"}
     
     end
-    follow_redirect!
     assert_template "users/show"  
   end
 
