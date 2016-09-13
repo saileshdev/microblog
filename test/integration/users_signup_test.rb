8a3251d0e84b7ad2c153ff86e840efaad3981c79
D.Sailesh Dev <saileshdev@gmail.com>
2016-09-13 21:21:59 +0530

    Refactor asser_equal with assert_no_difference

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -4,11 +4,13 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   
   test "invalid signup information" do
     get signup_path
-    before_count = User.count
-    
+   
+    assert_no_difference 'User.count' do 
     post users_path, user: {name: "",email: "user@invalid", password: "foo", password_confirmation: "bar"}
-    after_count = User.count
-    assert_equal before_count, after_count
+    
+    end
+    
     assert_template "users/new"  
   end
+
 end
