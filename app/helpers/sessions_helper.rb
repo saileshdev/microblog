93d84963194f185f3dbe30cbbe585b710f1887bc
D.Sailesh Dev <saileshdev@gmail.com>
2016-09-14 21:37:12 +0530

    Logout the user

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -14,4 +14,12 @@ module SessionsHelper
   def logged_in?
     !current_user.nil?
   end
+
+  # logout the user
+  def log_out
+    session[:user_id] = nil
+    # or we can do, session.delete(:user_id)
+    @current_user = nil
+  end
+
 end
