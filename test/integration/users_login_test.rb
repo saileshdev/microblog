ef704a2c4fe8ec92e37062dbd88cad083fff74a7
D.Sailesh Dev <saileshdev@gmail.com>
2016-09-14 19:35:36 +0530

    Add integration test for user's login

---
+++ test/integration/users_login_test.rb
@@ -0,0 +1,12 @@
+require 'test_helper'
+
+class UsersLoginTest < ActionDispatch::IntegrationTest
+  
+  test "login with invalid information" do
+    get login_path
+    assert_template "sessions/new"
+    post login_path, session: {email: "", password: ""}  
+    assert_template "sessions/new"
+    assert_not flash.empty?
+  end
+end
