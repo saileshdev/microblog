ad52b7b9aeeb77f2cda8095387750dea5c0c695f
D.Sailesh Dev <saileshdev@gmail.com>
2016-09-14 10:33:33 +0530

    Add routes for the session controller

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,5 @@ Rails.application.routes.draw do
 
   get    "login" => "sessions#new"
   post   "login" => "sessions#create"
-  delete "login" => "sessions#destroy"
+  delete "logout" => "sessions#destroy"
 end
