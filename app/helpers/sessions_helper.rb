6ef73a3830b7b7fe9993b4c0d87db823677cf161
D.Sailesh Dev <saileshdev@gmail.com>
2016-09-14 20:02:31 +0530

    Make the memoization patch idiomatic

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -7,10 +7,6 @@ module SessionsHelper
 
   #returns the current logged-in user
   def current_user
-    if @current_user.nil?
-      @current_user = User.find_by(id: sessions[:user_id])
-    else
-      @current_user
-    end
+      @current_user || = User.find_by(id: sessions[:user_id])
   end
 end
