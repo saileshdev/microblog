96338de086d9fff081bd0eb805e5cc1f06779efd
D.Sailesh Dev <saileshdev@gmail.com>
2016-09-14 19:57:54 +0530

    Get the current user from the sessions hash

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,6 +1,12 @@
 module SessionsHelper
 
+  # logs in the user
   def log_in(user)
     session[:user_id] = user.id
+  end
+
+  #returns the current logged-in user
+  def current_user
+    User.find_by(id: sessions[:user_id])
   end
 end
