7ccb6926d6f40381dfe68eb4e4c49650f07d16a5
D.Sailesh Dev <saileshdev@gmail.com>
2016-09-14 19:55:45 +0530

    Adds the user_id to the sessions hash

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,8 @@ class SessionsController < ApplicationController
     user = User.find_by_email(params[:session][:email])
     if user && user.authenticate(params[:session][:password])
       # log the user in and redirect the to the user's show page
+      log_in user
+      redirect_to user
     else
       # create an error message
       flash.now[:danger] = "Invalid email/password combination"
