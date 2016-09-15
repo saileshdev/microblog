d7cd7e68aaa72bc7b029e820ac07cb98512adbe4
D.Sailesh Dev <saileshdev@gmail.com>
2016-09-15 19:23:53 +0530

    Add integration test for editing users

---
+++ test/integration/users_edit_test.rb
@@ -0,0 +1,11 @@
+require 'test_helper'
+
+class UsersEditTest < ActionDispatch::IntegrationTest
+  
+  def setup
+    @user = users(:valid_user)
+  end
+  # test "the truth" do
+  #   assert true
+  # end
+end
