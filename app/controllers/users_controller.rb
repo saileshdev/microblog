3b8e8e1bf14d31294d55175a94ea4c629991e595
D.Sailesh Dev <saileshdev@gmail.com>
2016-09-15 19:48:51 +0530

    Authorize protected pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  
+  before_action :logged_in_user, only: [:edit, :update]
+
   def new
     @user = User.new
   end
@@ -41,5 +44,7 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name,:email,:password,:password_confirmation)
   end
+
+
 
 end
