268f6bed0daabece23ea94134db13e8f6ffea168
D.Sailesh Dev <saileshdev@gmail.com>
2016-09-15 20:00:45 +0530

    Assert that page is redirected to login for edit action if not logged in

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,19 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
+  
+  def setup
+    @user = users(:valid_user)
+  end
+
   test "should get new" do
     get :new
     assert_response :success
+  end
+
+  test "should redirect edit when not logged in" do
+    get :edit, id: @user.id
+    assert_redirected_to login_url
   end
 
 end
