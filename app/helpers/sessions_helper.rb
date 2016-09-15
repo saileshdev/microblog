24550da9dcd7737a1d25fa66391d6fb7efea7a30
D.Sailesh Dev <saileshdev@gmail.com>
2016-09-15 10:21:52 +0530

    Make if clauses idiomatic

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -15,12 +15,12 @@ module SessionsHelper
 
   #returns the current logged-in user
   def current_user
-    if session[:user_id]
+    if (user_id = session[:user_id])
       #if sessions still exists case
-      @current_user ||= User.find_by(id: session[:user_id])
-    elsif cookies.signed[:user_id]
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
       # if the session is lost, but persistent cookies still exist case
-      user = User.find_by(id: cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
       if user && user.authenticated?(cookies[:remember_token])
         log_in user
         @current_user = user
