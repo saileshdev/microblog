63e37b759ede5f03e95e18f3cd77e60b2351b4c5
D.Sailesh Dev <saileshdev@gmail.com>
2016-09-15 19:27:10 +0530

    Add test for unsuccessful editing

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -5,7 +5,11 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   def setup
     @user = users(:valid_user)
   end
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test "unsuccessful edit" do
+    get edit_user_path(@user)
+    patch user_path(@user), user: {name: "",email: "user@invalid", password: "foo", password_confirmation: "bar"}
+    assert_template "users/edit" 
+  end
+
 end
