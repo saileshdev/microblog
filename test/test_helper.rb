20d412d07a831cb45d8df5b111d259d2b87c56b5
D.Sailesh Dev <saileshdev@gmail.com>
2016-09-15 16:36:33 +0530

    Add a test helper method to log in the user depending on the nature of the test, meaning whetehr its integration test or not

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -12,4 +12,25 @@ class ActiveSupport::TestCase
   def is_logged_in?
     !session[:user_id].nil?
   end
+
+  # returns true if a test user is logged in
+  def log_in_as(user,options = {})    
+    #by default remember me is set to 1
+    remember_me = options[:remember_me] || "1"
+    password = options[:password] || "password"
+
+    if integration_test?
+      post login_path, session: {email: user.email, password: password, remember_me: remember_me}
+    else
+      session[:user_id] = user.id
+    end
+
+  end
+
+  private
+    
+    # returns true inside an integration test
+    def integration_test?
+      defined?(post_via_redirect)
+    end
 end
