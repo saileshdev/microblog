9b7d45b2384a3aef71d542cf22784ed29961da01
D.Sailesh Dev <saileshdev@gmail.com>
2016-09-15 20:28:50 +0530

    Remove redundant user finding in edit and update actions as before_action :correct_user is doing the same

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,11 +26,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update 
-    @user = User.find(params[:id])
     if @user.update_attributes(user_params)
       # handle successful update
       flash[:success] = "Profile updated"
