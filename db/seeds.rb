d0fc883b614d9bb1565cd9f8b771b7879e43b0fa
D.Sailesh Dev <saileshdev@gmail.com>
2016-09-16 10:31:19 +0530

    Add fake users in db/seed.rb file to populate the dev database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create!(name: "Admin User", email: "admin@user.com", password: "adminpassword", password_confirmation: "adminpassword")
+
+99.times do |n|
+  name = Faker::Name.name
+  email = "example-#{n+1}@user.com"
+  password = "password"
+  User.create!(name: name, email: email, password: password, password_confirmation: password)
+end
