e1dfd544a4ab006870ed483a701e062e2e573f49
D.Sailesh Dev <saileshdev@gmail.com>
2016-09-16 10:54:55 +0530

    Add users test data in fixtures

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class UsersIndexTest < ActionDispatch::IntegrationTest
+  
+  def setup
+    @users = users(:valid_user)
+  end
   # test "the truth" do
   #   assert true
   # end
