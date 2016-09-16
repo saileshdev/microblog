70bb7b9294b0b6ef5190fc98b0c02077b7c8cdc5
D.Sailesh Dev <saileshdev@gmail.com>
2016-09-16 11:09:31 +0530

    Adds test code for pagination

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -3,9 +3,17 @@ require 'test_helper'
 class UsersIndexTest < ActionDispatch::IntegrationTest
   
   def setup
-    @users = users(:valid_user)
+    @user = users(:valid_user)
   end
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test "index including pagination" do
+    log_in_as(@user)
+    get users_path
+    assert_template "users/index"
+    assert_select "div.pagination"
+    User.paginate(page: 1).each do |user|
+      assert_select "a[href=?]", user_path(user), text: user.name
+    end
+  end
+
 end
