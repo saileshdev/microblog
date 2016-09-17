d23a06676e012b7f9d43c0909008e48dee444784
D.Sailesh Dev <saileshdev@gmail.com>
2016-09-17 15:49:22 +0530

    Refactor downcase_email

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   # creates a virtual attribute remember_token which exists only in memory and not in db
   attr_accessor :remember_token, :activation_token
 
-  before_save {self.email = email.downcase}
+  before_save :downcase_email 
   before_create :create_activation_digest
 
   VALID_EMAIL_REGEX = /[\w.+\-]+@[a-z\-.]+\.[a-z]+/i
@@ -52,5 +52,10 @@ class User < ActiveRecord::Base
   def create_activation_digest
     self.activation_token = User.new_token
     self.activation_digest = User.digest(activation_token)
+  end
+
+  #converts email to all lower-case
+  def downcase_email
+    self.email = email.downcase
   end
 end
