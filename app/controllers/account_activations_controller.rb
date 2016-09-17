1c06881c65155083bca054c20993c752d32107b1
D.Sailesh Dev <saileshdev@gmail.com>
2016-09-17 17:21:46 +0530

    Define edit acyion for AccountActivations controller

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -1,2 +1,12 @@
 class AccountActivationsController < ApplicationController
+
+  def edit
+    user = User.find_by(email: params[:email])
+    if user && user.authenticated?(:activation, params[:id])
+      #activate the user
+    else
+      #invalid activation link
+    end
+
+  end
 end
