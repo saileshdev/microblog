75dcb33c47e4c520cfe1678eaaf579cba1de2213
D.Sailesh Dev <saileshdev@gmail.com>
2016-09-17 17:30:54 +0530

    Call the authenticated? method with attribute to pass test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -73,7 +73,7 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "authenticated? should return false for a user with nil digest" do
-    assert_not @user.authenticated?("some_token_value")
+    assert_not @user.authenticated?(:remember, "some_token_value")
   end
 
 end
