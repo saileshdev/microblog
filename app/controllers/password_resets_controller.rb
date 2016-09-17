7964be74f84f6f283bfd2853d7e452d1bd5cecec
D.Sailesh Dev <saileshdev@gmail.com>
2016-09-17 23:30:14 +0530

    Define create action in PasswordResets controller

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -2,6 +2,18 @@ class PasswordResetsController < ApplicationController
   def new
   end
 
-  def edit
+  def create
+    @user = User.find_by(email: params[:password_reset][:email].downcase)
+    if @user
+      #if email exists in db then create the digest
+      @user.create_user_digest
+      @user.send_password_reset_email
+      flash[:info] = "Email sent with password reset instructions"
+      redirect_to root_url
+    else
+      #when email isnt found in the database
+      flash.now[:danger] = "Email address not found"
+      render 'new'
+    end
   end
 end
