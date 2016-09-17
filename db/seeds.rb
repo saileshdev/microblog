e690250fbc7be94b6263f36f61f0ce3dcc87c604
D.Sailesh Dev <saileshdev@gmail.com>
2016-09-17 15:51:41 +0530

    Make the seed users activated

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,12 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.create!(name: "Admin User", email: "admin@user.com", password: "adminpassword", password_confirmation: "adminpassword", admin: true)
+User.create!(name: "Admin User", email: "admin@user.com", password: "adminpassword", password_confirmation: "adminpassword", 
+             admin: true, activated: true, activated_at: Time.zone.now)
 
 99.times do |n|
   name = Faker::Name.name
   email = "example-#{n+1}@user.com"
   password = "password"
-  User.create!(name: name, email: email, password: password, password_confirmation: password)
+  User.create!(name: name, email: email, password: password, password_confirmation: password, activated: true, activated_at: Time.zone.now)
 end
