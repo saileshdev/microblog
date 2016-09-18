4fd9462784642deeaae99ed9d1582f01ebc11961
D.Sailesh Dev <saileshdev@gmail.com>
2016-09-18 19:09:23 +0530

    Check reset_token expiration for edit action also

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -2,6 +2,7 @@ class PasswordResetsController < ApplicationController
   
   before_action :get_user, only: [:edit, :update] 
   before_action :valid_user, only: [:edit, :update]
+  before_action :check_expiration, only: [:edit, :update]
 
   def new
   end
@@ -25,10 +26,7 @@ class PasswordResetsController < ApplicationController
   end
 
   def update
-    if @user.password_reset_expired?
-      flash[:danger] = "Password reset has expired"
-      redirect_to new_password_reset_url
-    elsif @user.update_attributes(user_params)
+    if @user.update_attributes(user_params)
       log_in @user
       flash[:success] = "Password has been reset"
       redirect_to @user
@@ -51,6 +49,13 @@ class PasswordResetsController < ApplicationController
   def valid_user
   unless(@user && @user.activated? && @user.authenticated?(:reset,params[:id]))
       redirect_to root_url
+    end
+  end
+
+  def check_expiration
+    if @user.password_reset_expired?
+      flash[:danger] = "Password reset has expired"
+      redirect_to new_password_reset_url
     end
   end
 
