f1c3739408e5c7c195ff76d5bf9d021ad1221231
D.Sailesh Dev <saileshdev@gmail.com>
2016-09-18 16:42:50 +0530

    Clear ActionMailer::Base.deliveries before every test

--- test/integration/password_resets_test.rb
+++ test/integration/password_resets_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class PasswordResetsTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  def setup
+  #we are clearing the array before any test
+  ActionMailer::Base.deliveries.clear
+  #ActionMailer::Base.deliveries is an array and the length becomes 1 when a mail is sent 
+  end
+
+  
 end
