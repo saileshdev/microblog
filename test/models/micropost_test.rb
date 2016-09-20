1adfcab1812a3fc22789b1a89c3a5ef359423bdb
D.Sailesh Dev <saileshdev@gmail.com>
2016-09-20 19:54:53 +0530

    Test to assert user id should be present

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -3,13 +3,19 @@ require 'test_helper'
 class MicropostTest < ActiveSupport::TestCase
   
   def setup
-  @micropost = Micropost.new
+  @user = users(:valid_user)
+  @micropost = Micropost.new(user_id: @user.id)
   end
-
 
   test "should be valid" do
     assert @micropost.valid?
   end
+
+  test "user id should be present" do
+    @micropost.user_id = nil
+    assert_not @micropost.valid?
+  end
+
 
 
 end
