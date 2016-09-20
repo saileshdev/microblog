74159c283b36943f6bd24c50e3a946b4072fba62
D.Sailesh Dev <saileshdev@gmail.com>
2016-09-20 22:47:04 +0530

    Refactor code

--- test/integration/users_profile_test.rb
+++ test/integration/users_profile_test.rb
@@ -2,6 +2,8 @@ require 'test_helper'
 
 class UsersProfileTest < ActionDispatch::IntegrationTest
   
+  include ApplicationHelper  
+  
   def setup
     @user = users(:valid_user)
   end
@@ -10,7 +12,7 @@ class UsersProfileTest < ActionDispatch::IntegrationTest
    get user_path(@user)
    assert_template 'users/show'
 
-  assert_select 'title', "#{@user.name} | Microblog" 
+  assert_select 'title', full_title(@user.name) 
   end
 
 end
