a773159bb06139c1d3f6600bbf2d0ab1f9582eab
D.Sailesh Dev <saileshdev@gmail.com>
2016-09-20 23:06:55 +0530

    Setup @micropost for micropost controller tests

--- test/controllers/microposts_controller_test.rb
+++ test/controllers/microposts_controller_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class MicropostsControllerTest < ActionController::TestCase
+  
+  def setup
+    @microposts = microposts(:orange)
+  end
+
   # test "the truth" do
   #   assert true
   # end
