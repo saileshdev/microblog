33e97acba3ee6e73f2b13456b40492cdab1bd529
D.Sailesh Dev <saileshdev@gmail.com>
2016-09-20 20:09:02 +0530

    Use user.microposts.build to refactror code

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -4,7 +4,7 @@ class MicropostTest < ActiveSupport::TestCase
   
   def setup
   @user = users(:valid_user)
-  @micropost = Micropost.new(user_id: @user.id, content: "Lorem ipsum")
+  @micropost = @user.microposts.build(content: "Lorem ipsum")
   end
 
   test "should be valid" do
