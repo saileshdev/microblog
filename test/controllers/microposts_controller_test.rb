9bf8eb537eadf61ee2e0d297f4bc12c020d65820
D.Sailesh Dev <saileshdev@gmail.com>
2016-09-20 23:24:10 +0530

    Redirect ot login url instead of root_url

--- test/controllers/microposts_controller_test.rb
+++ test/controllers/microposts_controller_test.rb
@@ -3,21 +3,21 @@ require 'test_helper'
 class MicropostsControllerTest < ActionController::TestCase
   
   def setup
-    @microposts = microposts(:orange)
+    @micropost = microposts(:orange)
   end
 
   test "should redirect create when not logged in" do
     assert_no_difference "Micropost.count" do
        post :create, micropost: {content: "Lorem ipsum"}
     end
-    assert_redirected_to root_url 
+    assert_redirected_to login_url 
   end
 
   test "should redirect destroy when not logged in" do
     assert_no_difference "Micropost.count" do
        delete :destroy, id: @micropost
     end
-    assert_redirected_to root_url 
+    assert_redirected_to login_url
   end
 
 end
