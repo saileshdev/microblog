3f4ce59a3fe5ea2a3f07a4f2092c6fc5dc324c2f
D.Sailesh Dev <saileshdev@gmail.com>
2016-09-20 19:51:42 +0530

    Define setup for micropost model tests

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -2,6 +2,11 @@ require 'test_helper'
 
 class MicropostTest < ActiveSupport::TestCase
   
+  def setup
+  @micropost = Micropost.new
+  end
+
+
   test "should be valid" do
     assert @micropost.valid?
   end
