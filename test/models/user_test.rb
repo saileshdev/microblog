837d0d6dbc0cc047c06c974692c87e24f10a0221
D.Sailesh Dev <saileshdev@gmail.com>
2016-09-20 20:35:11 +0530

    Add test code to check is associated microposts are deleted if the user is destroyed

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -76,4 +76,13 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.authenticated?(:remember, "some_token_value")
   end
 
+  test "associated microposts should be destroyed" do
+    #@user is just created in memory so lets save it to db
+    @user.save
+    @user.microposts.create!(content: "Lorem ipsum")
+    assert_difference "Micropost.count", -1 do
+      @user.destroy
+    end
+  end
+ 
 end
