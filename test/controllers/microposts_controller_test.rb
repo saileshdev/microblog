d878213dae77deea62d99fce78c48a5700fd1dbd
D.Sailesh Dev <saileshdev@gmail.com>
2016-09-22 18:44:39 +0530

    Assert that user is redirected if wrong micropost is deleted

--- test/controllers/microposts_controller_test.rb
+++ test/controllers/microposts_controller_test.rb
@@ -19,5 +19,12 @@ class MicropostsControllerTest < ActionController::TestCase
     end
     assert_redirected_to login_url
   end
+  
+  test "should redirect destroy for wrong micropost" do
+    log_in_as(users(:valid_user))
+    assert_no_difference "Micropost.count" do
+       delete :destroy, id: microposts(:ants)
+    end
+  end  
 
 end
