ee2c5ebab841fbf3b34493657c94ceaa6384a896
D.Sailesh Dev <saileshdev@gmail.com>
2016-09-22 18:48:07 +0530

    Pull user from fixtures

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
+  
+  def setup
+    @user = users(:valid_user)
+  end
+
   # test "the truth" do
   #   assert true
   # end
