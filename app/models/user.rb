56fb24a23211e9952f98253ce639afc0c6e428f2
D.Sailesh Dev <saileshdev@gmail.com>
2016-09-22 17:55:34 +0530

    Add feed method in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -76,6 +76,11 @@ class User < ActiveRecord::Base
   def password_reset_expired?
    self.reset_sent_at < 2.hours.ago 
   end
+  
+  #defines a proto-feed
+  def feed
+   Micropost.where("user_id=?",id)
+  end 
 
   private
   
